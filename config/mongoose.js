dd2e990c533e93d0fafee9182bb79ab53488a275
Nayan Agarwal <69133787+the-wormhole@users.noreply.github.com>
2021-11-11 20:17:45 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 //Setting up MongoDB 
 
 mongoose.connect(process.env.MONGO_ATLAS,{useUnifiedTopology: true}); 
-//mongodb+srv://NayanAgarwal:<password>@cluster0.tgf4z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-//mongodb://localhost/FeedBack-System
+
 const db = mongoose.connection;
 
 db.on("error",console.error.bind(console,"Error in connecting to MongoDB!"));
